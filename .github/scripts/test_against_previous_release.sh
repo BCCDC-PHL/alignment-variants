1af37e9eb1da19aa5ec9f601d2dda6787c70e782
Dan Fornika <dfornika@gmail.com>
2023-11-23 15:16:04 -0800

    Update test_against_previous_release.sh

--- .github/scripts/test_against_previous_release.sh
+++ .github/scripts/test_against_previous_release.sh
@@ -35,7 +35,7 @@ cp -r output artifacts/pull_request_results
 # run tests against previous previous_release to compare outputs 
 git clone https://github.com/dfornika/alignment-variants.git previous_release 
 cd previous_release
-git checkout 17c7b2ea3f5aa188f5e7fc9f4d9c1fea1e8d2515
+git checkout a2ed2f35e7113897e0142dbb317ee7aa7e5fc609
 
 # the github runner only has 2 cpus available, so replace for that commit required:
 sed -i s'/cpus = 24/cpus = 2/'g nextflow.config
