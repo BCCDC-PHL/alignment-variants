15db2c2cce65f802e68c6cb53f83a23badcdcb18
Dan Fornika <dfornika@gmail.com>
2023-11-27 13:17:03 -0800

    collect more qualimap metrics

--- bin/qualimap_bamqc_genome_results_to_csv.py
+++ bin/qualimap_bamqc_genome_results_to_csv.py
@@ -13,15 +13,56 @@ def main(args):
         }
         for line in f:
             line = line.strip()
+            if line.startswith('number of reads'):
+                number_of_reads = line.split('=')[1].strip().replace(',', '')
+                output_data['num_reads'] = int(number_of_reads)
+            if line.startswith('number of mapped reads'):
+                num_mapped_reads = line.split('=')[1].strip().split(' ')[0].replace(',', '')
+                output_data['num_mapped_reads'] = int(num_mapped_reads)
+                percent_mapped_reads = line.split('=')[1].strip().split(' ')[1].strip().replace('(', '').replace(')', '').replace('%', '')
+                output_data['percent_mapped_reads'] = round(float(percent_mapped_reads), 2)
+            if line.startswith('number of secondary alignments'):
+                num_secondary_alignments = int(line.split('=')[1].strip().replace(',', ''))
+                output_data['num_secondary_alignments'] = num_secondary_alignments
+            if line.startswith('mean insert size'):
+                mean_insert_size = line.split('=')[1].strip().replace(',', '')
+                output_data['mean_insert_size'] = float(mean_insert_size)
             if line.startswith('median insert size'):
                 median_insert_size = line.split('=')[1].strip().replace(',', '')
-                output_data['median_insert_size'] = int(median_insert_size)
+                output_data['median_insert_size'] = float(median_insert_size)
+            if line.startswith('std insert size'):
+                stdev_insert_size = line.split('=')[1].strip().replace(',', '')
+                output_data['stdev_insert_size'] = float(stdev_insert_size)
+            if line.startswith('duplication rate'):
+                duplication_rate = line.split('=')[1].strip().replace('%', '')
+                output_data['duplication_rate_percent'] = round(float(duplication_rate), 2)
             if line.startswith('mean coverageData'):
                 mean_coverage = line.split('=')[1].strip().strip('X').replace(',', '')
-                output_data['mean_coverage'] = round(float(mean_coverage), 2)
+                output_data['mean_depth_coverage'] = round(float(mean_coverage), 2)
             if line.startswith('std coverageData'):
                 stdev_coverage = line.split('=')[1].strip().strip('X').replace(',', '')
-                output_data['stdev_coverage'] = round(float(stdev_coverage), 2)
+                output_data['stdev_depth_coverage'] = round(float(stdev_coverage), 2)
+            if line.startswith('mean mapping quality'):
+                mean_mapping_quality = line.split('=')[1].strip()
+                output_data['mean_mapping_quality'] = round(float(mean_mapping_quality), 2)
+            if line.startswith('general error rate'):
+                general_error_rate = line.split('=')[1].strip()
+                output_data['error_rate'] = round(float(general_error_rate), 2)
+            if line.startswith('number of mismatches'):
+                number_of_mismatches = line.split('=')[1].strip()
+                output_data['number_of_mismatches'] = int(number_of_mismatches)
+            if line.startswith('number of insertions'):
+                number_of_insertions = line.split('=')[1].strip()
+                output_data['number_of_insertions'] = int(number_of_insertions)
+            if line.startswith('mapped reads with insertion percentage'):
+                mapped_reads_with_insertion_percentage = line.split('=')[1].strip().replace('%', '')
+                output_data['mapped_reads_with_insertion_percentage'] = round(float(mapped_reads_with_insertion_percentage), 2)
+            if line.startswith('number of deletions'):
+                number_of_deletions = line.split('=')[1].strip()
+                output_data['number_of_deletions'] = int(number_of_deletions)
+            if line.startswith('mapped reads with deletion percentage'):
+                mapped_reads_with_deletion_percentage = line.split('=')[1].strip().replace('%', '')
+                output_data['mapped_reads_with_deletion_percentage'] = round(float(mapped_reads_with_deletion_percentage), 2)
             if 'reference with a coverageData >= 5X' in line:
                 proportion_genome_covered_over_5x = float(line.split(' ')[3].strip('%')) / 100
                 output_data['proportion_genome_covered_over_5x'] = round(proportion_genome_covered_over_5x, 4)
@@ -45,9 +86,23 @@ def main(args):
     output_fieldnames = [
         'sample_id',
         'read_type',
+        'mean_depth_coverage',
+        'stdev_depth_coverage',
+        'num_reads',
+        'num_mapped_reads',
+        'percent_mapped_reads',
+        'mean_insert_size',
         'median_insert_size',
-        'mean_coverage',
-        'stdev_coverage',
+        'stdev_insert_size',
+        'mean_mapping_quality',
+        'error_rate',
+        'number_of_mismatches',
+        'number_of_insertions',
+        'mapped_reads_with_insertion_percentage',
+        'number_of_deletions',
+        'mapped_reads_with_deletion_percentage',
+        'num_secondary_alignments',
+        'duplication_rate_percent',
         'proportion_genome_covered_over_5x',
         'proportion_genome_covered_over_10x',
         'proportion_genome_covered_over_20x',
@@ -56,8 +111,7 @@ def main(args):
         'proportion_genome_covered_over_50x',
     ]
 
-    csv.register_dialect('unix-csv', delimiter=',', doublequote=False, lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
-    writer = csv.DictWriter(sys.stdout, fieldnames=output_fieldnames, dialect='unix-csv')
+    writer = csv.DictWriter(sys.stdout, fieldnames=output_fieldnames, dialect='unix', extrasaction='ignore', quoting=csv.QUOTE_MINIMAL)
     writer.writeheader()
     writer.writerow(output_data)
 
